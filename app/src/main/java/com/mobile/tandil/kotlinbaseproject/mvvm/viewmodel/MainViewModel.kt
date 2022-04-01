6c22ad7323dd6c4e35c7fab5c096246e5d4dac91
florenciaferraiolo <florencia.ferraiolo@globant.com>
2022-04-01 15:57:23 -0300

    MVVM Structure
    requested changes

---
+++ app/src/main/java/com/mobile/tandil/kotlinbaseproject/mvvm/viewmodel/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.mobile.tandil.kotlinbaseproject.mvvm.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val mutableLiveData: MutableLiveData<CheckData> = MutableLiveData()
+
+    fun getValue(): MutableLiveData<CheckData> {
+        return mutableLiveData
+    }
+
+    fun initValue() {
+        mutableLiveData.value = CheckData(CheckState.INITIAL)
+    }
+
+    data class CheckData(
+        val state: CheckState = CheckState.INITIAL
+    )
+
+    enum class CheckState {
+        INITIAL
+    }
+}
